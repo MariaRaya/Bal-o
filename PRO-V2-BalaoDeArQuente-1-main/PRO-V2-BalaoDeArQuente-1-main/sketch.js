7b38279a2feb869f0ab6f3db722277fabe252e65
MariaRaya <94574813+MariaRaya@users.noreply.github.com>
2022-05-12 21:59:44 -0300

    Add files via upload

--- PRO-V2-BalaoDeArQuente-1-main/PRO-V2-BalaoDeArQuente-1-main/sketch.js
+++ PRO-V2-BalaoDeArQuente-1-main/PRO-V2-BalaoDeArQuente-1-main/sketch.js
@@ -2,11 +2,13 @@ var bg, bgImg
 var bottomGround
 var topGround
 var balloon, balloonImg
-var gameState = 1;
-var button;
+var gameState = 0;
+var button, buttonImg;
 var obsTop, obsTop1, obsTop2, obsBottom, obsBottom1, obsBottom2, obsBottom3;
 var restart, fimdeJogo, fimdeJogoImg, restartImg;
 var bottomGroup, topGroup;
+var score = 0;
+var sound1, sound2;
 
 function preload(){
 bgImg = loadImage("assets/bg.png")
@@ -19,6 +21,9 @@ obsBottom2 = loadImage("assets/obsBottom2.png");
 obsBottom3 = loadImage("assets/obsBottom3.png");
 restartImg = loadImage("assets/restart.png");
 fimdeJogoImg = loadImage("assets/fimdejogo.png");
+buttonImg = loadImage("assets/button.png");
+sound1 = loadSound("assets/die.mp3");
+sound2 = loadSound("assets/jump.mp3");
 }
 
 function setup(){
@@ -29,6 +34,8 @@ function setup(){
 bg = createSprite(165,485,1,1);
 bg.addImage(bgImg);
 bg.scale = 1.3
+
+bg.x = bg.width / 2;
 
 //criando canto superior e inferior
 bottomGround = createSprite(200,390,800,20);
@@ -41,18 +48,26 @@ topGround.visible = false;
 balloon = createSprite(100,200,20,50);
 balloon.addAnimation("balloon",balloonImg);
 balloon.scale = 0.15;
+balloon.setCollider("circle", 0,0,150);
+balloon.debug = false;
 
 //fim de jogo  
 fimdeJogo = createSprite(200,200,20,50);
 fimdeJogo.addImage(fimdeJogoImg);
-fimdeJogo.scale = 0.5;
+fimdeJogo.scale = 0.2
 fimdeJogo.visible = false;
 
 //fim de jogo  
-restart = createSprite(200,240,20,50);
+restart = createSprite(200,285,20,50);
 restart.addImage(restartImg);
-restart.scale = 0.5;
+restart.scale = 0.2;
 restart.visible = false;
+
+//Botão de Início
+button = createSprite(200,200);
+button.addImage(buttonImg);
+button.scale = 0.2;
+
 
 topGroup = new Group();
 bottomGroup = new Group();
@@ -64,19 +79,26 @@ function draw() {
   background("black");
         
     if (gameState == 0){
-      text("Pressione o botão para iniciar", 70, height/2);
-      /*button = createImg("assets/button.png");
-      button.position(200,200);
-      button.size(50,50);
-      button.mouseClicked(iniciar);*/
-
+      button.visible = true;
+      if(mousePressedOver(button)){
+        iniciar();
+      }
     }
 
     if (gameState == 1){
       //fazendo o balão de ar quente pular
       if(keyDown("space")) {
         balloon.velocityY = -6 ;
+        sound2.play();
         
+      }
+
+      score = score + Math.round(getFrameRate()/60);
+
+      bg.velocityX = -2;
+
+      if(bg.x < 0){
+        bg.x = bg.width / 2;
       }
 
       //adicionando gravidade
@@ -88,10 +110,15 @@ function draw() {
         if(topGroup.isTouching(balloon) || bottomGroup.isTouching(balloon) || 
         balloon.isTouching(bottomGround) ||  balloon.isTouching(topGround)){
           gameState = 2;
+          sound1.play();
+          sound1.setVolume(0.5);
         }
     }
           
     if (gameState == 2){
+
+      bg.velocityX = 0;
+
       fimdeJogo.visible = true;
       fimdeJogo.depth = fimdeJogo.depth + 1;
 
@@ -113,11 +140,13 @@ function draw() {
     }
    
         drawSprites();
+        Score();
         
 }
 
 function iniciar(){
   gameState = 1;
+  button.visible = false;
 }
 
 function gerarObsTop(){
@@ -166,11 +195,19 @@ function gerarObsBottom(){
 }
 
 function reset(){
-  gameState = 1;
+  gameState = 0;
+  score = 0;
   fimdeJogo.visible = false;
   restart.visible = false;
   topGroup.destroyEach();
   bottomGroup.destroyEach();
+}
+
+function Score(){
+ // textFont("algerian");
+  textSize(20);
+  fill("black");
+  text("Pontuação: " + score, 250, 50);
 }
 
 
